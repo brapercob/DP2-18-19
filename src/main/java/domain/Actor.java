2e6d5d288b7c5036799b1a74081d5232c00fce2a
Diego <petakaaracena@gmail.com>
2019-02-24 13:18:58 +0100

    Diego Primeras clases

---
+++ src/main/java/domain/Actor.java
@@ -0,0 +1,139 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CascadeType;
+
+import security.UserAccount;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public abstract class Actor extends DomainEntity {
+
+	private String						name;
+	private String						middleName;
+	private String						surname;
+	private String						photo;
+	private String						email;
+	private String						phoneNumber;
+	private String						address;
+	public Collection<SocialProfile>	socialProfiles;
+	public UserAccount					userAccount;
+	public Collection<MessageBox>		messageBoxes;
+	public Collection<Message>			messagesSent;
+	public Collection<Message>			messagesRecipient;
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(final String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(final String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhoto() {
+		return this.photo;
+	}
+
+	public void setPhoto(final String photo) {
+		this.photo = photo;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(final String address) {
+		this.address = address;
+	}
+
+	@OneToMany
+	public Collection<SocialProfile> getSocialProfiles() {
+		return this.socialProfiles;
+	}
+
+	public void setSocialProfiles(final Collection<SocialProfile> socialProfiles) {
+		this.socialProfiles = socialProfiles;
+	}
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
+	@OneToMany
+	public Collection<MessageBox> getMessageBoxes() {
+		return this.messageBoxes;
+	}
+
+	public void setMessageBoxes(final Collection<MessageBox> messageBoxes) {
+		this.messageBoxes = messageBoxes;
+	}
+
+	@OneToMany(mappedBy = "sender")
+	public Collection<Message> getMessagesSent() {
+		return this.messagesSent;
+	}
+
+	public void setMessagesSent(final Collection<Message> messagesSent) {
+		this.messagesSent = messagesSent;
+	}
+
+	@OneToMany(mappedBy = "recipient")
+	public Collection<Message> getMessagesRecipient() {
+		return this.messagesRecipient;
+	}
+
+	public void setMessagesRecipient(final Collection<Message> messagesRecipient) {
+		this.messagesRecipient = messagesRecipient;
+	}
+
+}
---
+++ src/main/java/domain/Memer.java
@@ -0,0 +1,37 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Memer extends Actor {
+
+	public Collection<Enrolment>	enrolments;
+	public Collection<Request>		requests;
+
+
+	@OneToMany
+	public Collection<Enrolment> getEnrolments() {
+		return this.enrolments;
+	}
+
+	public void setEnrolments(final Collection<Enrolment> enrolments) {
+		this.enrolments = enrolments;
+	}
+
+	@OneToMany
+	public Collection<Request> getRequests() {
+		return this.requests;
+	}
+
+	public void setRequests(final Collection<Request> requests) {
+		this.requests = requests;
+	}
+
+}
